b8733c651b40c608668d141902d3ef1318a225b1
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2023-08-19 09:05:31 +0530

    tst

---
+++ CondSt/two.js
@@ -0,0 +1,8 @@
+let num = 33
+
+if(num %2 ===0){
+console.log("Even")
+}
+else{
+    console.log("odd")
+}
---
+++ Operators/one.js
@@ -0,0 +1,27 @@
+let a = 10
+let b= 20
+let c = 30.5
+let d = '40'
+let e = "forty"
+let f = "Rahul"
+let g = true
+let i;
+
+console.log(a+b)
+console.log(a+c)
+console.log(a+d)
+console.log(a+e)
+console.log(a+f)
+console.log(a+g)
+console.log(a+i)
+
+
+console.log("Multiplication")
+
+console.log(a*b)
+console.log(a*c)
+console.log(a*d)
+console.log(a*e)
+console.log(a*f)
+console.log(a*g)
+console.log(a*i)
