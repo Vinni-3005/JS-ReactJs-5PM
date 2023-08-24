d78ad3f724cc02fd51711ee10a386412560e3ef4
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2023-08-24 22:33:34 +0530

    okie

---
+++ DOM1/data.js
@@ -0,0 +1,9 @@
+
+document.getElementById('abc').innerHTML=[10,20,30]
+
+//document.getElementById('abc').innerHTML= new Date().toLocaleTimeString()
+
+/* let ref = document.getElementById('abc')
+
+console.log(ref)
+ref.innerHTML="Hello,GE" */
---
+++ DOM2/data.js
@@ -0,0 +1,10 @@
+function change_Name(){
+    //alert('ge')
+    document.getElementById('xyz').innerHTML="Rajnikanth"
+}
+function change_Color(){
+    //alert('ge')
+    document.getElementById('abc').style.backgroundColor="green"
+    //document.getElementById('abc').style.backgroundColor='green'
+    //document.getElementById('abc').style.fontStyle="italic"
+}
